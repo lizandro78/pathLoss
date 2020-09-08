b759aa1860c5677b2f85a7e37e98b68e6b942e3d
Lizandro <prof.lizandro.silva@gmail.com>
2020-09-08 18:13:12 -0300

    Typing / notes / plot error.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Importing libs
-
+from ctypes import Array
 from datetime import datetime
 from math import sqrt
+from typing import List, Dict, Tuple
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,6 +10,8 @@ import seaborn as sns
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
+from pandas import DataFrame
+
 
 from Cdb import CdbFactory
 from Imports import ImportHelper
@@ -17,16 +20,16 @@ from knnRegressor import MultiRegressor
 #%%
 #Getting Measurements
 
-df_meds = pd.read_csv("./dataIn/meds_pandas.csv")
-targets = ['rssi_1_1', 'rssi_1_2', 'rssi_1_3', 'rssi_2_1', 'rssi_2_2', 'rssi_2_3', 'rssi_3_1', 'rssi_3_2', 'rssi_3_3']
-features = ['dist_1', 'dist_2', 'dist_3']
+df_meds: DataFrame = pd.read_csv("./dataIn/meds_pandas.csv")
+targets: List[str] = ['rssi_1_1', 'rssi_1_2', 'rssi_1_3', 'rssi_2_1', 'rssi_2_2', 'rssi_2_3', 'rssi_3_1', 'rssi_3_2', 'rssi_3_3']
+features: List[str] = ['dist_1', 'dist_2', 'dist_3']
 
 
 #%%
 #Separate training and test
 #Obs.: Only outdoor to this case
 
-df_out = df_meds[df_meds['indoor'] == False]
+df_out: DataFrame = df_meds[df_meds['indoor'] == False]
 print(df_out.shape)  # 3064 x 43
 
 #%%
@@ -37,16 +40,17 @@ print("Longitude Min:{0} Max:{1}".format(df_out['lon'].min(), df_out['lon'].max(
 
 #%%
 #Separate features and targets
-
+train: DataFrame
+test: DataFrame
 train, test = train_test_split(df_out, test_size = 0.2)
-X_train = train[features] # dist1, 2  e 3 (column selection)
-X_test = test[features]
-Y_train = train[targets]
-Y_test = test[targets]
+X_train: DataFrame = train[features] # dist1, 2  e 3 (column selection)
+X_test: DataFrame = test[features]
+Y_train: DataFrame = train[targets]
+Y_test: DataFrame = test[targets]
 #%%
 #Create K-NN Modelfor each target (BTS)
 
-cv_parms = {'n_neighbors': list(range(1, 24))}
+cv_parms: Dict[str, List[int]] = {'n_neighbors': list(range(1, 24))}
 model = MultiRegressor(cv_parms=cv_parms, estimator=KNeighborsRegressor(weights='distance'))
 model.fit(X_train, Y_train)
 
@@ -60,8 +64,8 @@ for col in range(0, y_preds.shape[1]):
     print("RMSE Modelo {0}:{1} dB".format(col, sqrt(mean_squared_error(y_p, y_real))))
     #Root mean square error RMSE
     #Including values on the X_test
-    col_pred = 'RSSI_pred_{0}'.format(col)
-    col_real = 'RSSI_real_{0}'.format(col)
+    col_pred: str = 'RSSI_pred_{0}'.format(col)
+    col_real: str = 'RSSI_real_{0}'.format(col)
     X_test[col_pred] = y_p
     X_test[col_real] = y_real
 #%%
@@ -79,27 +83,27 @@ X_test.to_csv(file_name, index=True)
 #%%
 #Create a grid to execute predictions
 #Each point has a RSSI prediction
-bts = ImportHelper.GetBts()
-grid = CdbFactory.CreateCdb(df_meds["lat"].min(), df_meds["lon"].min(),
+bts: DataFrame = ImportHelper.GetBts()
+grid: DataFrame = CdbFactory.CreateCdb(df_meds["lat"].min(), df_meds["lon"].min(),
                             df_meds["lat"].max(),
                             df_meds["lon"].max(), 20)
 
 #%%
 #Mapping grid snd put on a file
-gridComDist = ImportHelper.PutDistancesFromBtss(bts, grid)
-coverMaps_arr = model.predict(gridComDist[features])
-coverMaps = pd.DataFrame(data=coverMaps_arr, columns=targets)
-coverMapsFinal = pd.concat([coverMaps, gridComDist], axis = 1)
+gridComDist: DataFrame = ImportHelper.PutDistancesFromBtss(bts, grid)
+coverMaps_arr: Array = model.predict(gridComDist[features])
+coverMaps: DataFrame = pd.DataFrame(data=coverMaps_arr, columns=targets)
+coverMapsFinal: DataFrame = pd.concat([coverMaps, gridComDist], axis = 1)
 coverMapsFinal.to_pickle("./dataOut/coverMapsFinal.pkl")
 
 #%%
 #Create a file to coverage with best BTS server
-map_data = pd.read_pickle("./dataOut/coverMapsFinal.pkl")
+map_data: DataFrame = pd.read_pickle("./dataOut/coverMapsFinal.pkl")
 map_data["best_server"] = map_data.drop(["dist_1","dist_2","dist_3","lat","lon"],axis=1).max(axis=1)
 
 #%%
 #Define coverage levels
-def cover_level(rssi):
+def cover_level(rssi)-> str:
     if rssi >= -65:
         return "Excellent"
     elif rssi >= -75:
@@ -119,15 +123,20 @@ map_data["nivel_cobertura"] = map_data["best_server"].apply(cover_level)
 #GEnerate the coverage plot
 
 #Build a box to plt results on the georeferenced map
-BBox = ((map_data["lon"].min()-0.002, map_data["lon"].max()+0.002, map_data["lat"].min()-0.002,map_data["lat"].max()+0.002))
+BBox: Tuple = ((map_data["lon"].min()-0.002, map_data["lon"].max()+0.002, map_data["lat"].min()-0.002,map_data["lat"].max()+0.002))
 mapGeoRef = plt.imread('./figs/map4.png')
 
 #plot the results over the georeferenced map
+#defining plot area
 fig, ax = plt.subplots(figsize = (8,7))
-sns.scatterplot(x = "lon", y = "lat", alpha= 0.75, ax =ax, legend='brief', zorder=1, data = map_data, hue = map_data["nivel_cobertura"].tolist())
+#plot coverage area
+sns.scatterplot(x = "lon", y = "lat", alpha= 0.85, ax =ax, legend='brief', zorder=1, data = map_data, hue = map_data["nivel_cobertura"].tolist())
+#Add Title
 ax.set_title('Coverage Map by RSSI level')
+#Add limits
 ax.set_xlim(BBox[0],BBox[1])
 ax.set_ylim(BBox[2],BBox[3])
+#georeferenced map
 ax.imshow(mapGeoRef, zorder=0, extent = BBox, aspect= 'equal')
 plt.show()
 
@@ -135,25 +144,15 @@ plt.show()
 
 #Creating Dataframe to RSSI analyses
 X_test.to_pickle("./dataOut/rssiReal.pkl")
-df_Rssi = pd.read_pickle("./dataOut/rssiReal.pkl")
+df_Rssi: DataFrame = pd.read_pickle("./dataOut/rssiReal.pkl")
 df_Rssi["index"] = range(1,614)
-df_Rssi["error"] = df_Rssi["RSSI_pred_8"] -  df_Rssi["RSSI_real_8"]
+df_Rssi["Error (dB)"] = df_Rssi["RSSI_pred_8"] -  df_Rssi["RSSI_real_8"]
 #%%
 # style
 plt.style.use('seaborn-darkgrid')
 
-# create a color palette
-palette = plt.get_cmap('Set1')
-
-# multiple line plot
-num = 0
-for column in df_Rssi.drop(["index","dist_1","dist_2","dist_3", "lat", "lon",
-                            "RSSI_pred_0","RSSI_real_0","RSSI_pred_1","RSSI_real_1",
-                            "RSSI_pred_2","RSSI_real_2","RSSI_pred_3","RSSI_real_3",
-                            "RSSI_pred_4","RSSI_real_4","RSSI_pred_5","RSSI_real_5",
-                            "RSSI_pred_6","RSSI_real_6","RSSI_pred_7","RSSI_real_7", "RSSI_pred_8","RSSI_real_8"], axis = 1):
-    num += 1
-    plt.plot(df_Rssi["index"], df_Rssi[column], marker='', color=palette(num), linewidth=1, alpha=0.9, label=column)
+# line plot
+plt.plot(df_Rssi["index"], df_Rssi["Error (dB)"], marker='', color="#336699", linewidth=1, alpha=0.9, label="Error (dB)")
 
 # Add legend
 plt.legend(loc=2, ncol=2)
